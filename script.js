b1be06f9bd15bad038044463c07b2143594ec471
margaux.begard <margaux.begard2@gmail.com>
2022-09-22 10:26:54 +0200

    modif java

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const message =
 "Merci pour votre message, notre royauté reviendra vers vous dans les plus brefs délais.";
 document
     .getElementById("contactForm")
-    .addEventListener("submit", function (event) {
+    .addEventListener("submit", function (event){
     event.preventDefault();
     alert(message);
 });
